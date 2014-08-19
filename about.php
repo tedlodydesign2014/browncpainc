9c392cc167dd245758aa668fc74c554f9bb5cb77
tedlodydesign2014 <tedlodydesign2014@gmail.com>
2014-08-18 22:18:36 -0500

    Brown CPA with BootStrap

---
+++ about.php
@@ -0,0 +1,8 @@
+<?php require_once ('include/top.php'); ?>
+
+
+<h1> About </h1>
+
+<?php
+	require_once ('include/bottom.php');
+?>
---
+++ include/top.php
@@ -0,0 +1,36 @@
+<html>
+<head>
+<link href="css/load.css" rel="stylesheet">
+
+<title>Brown CPA Inc.</title>
+</head>
+
+<body>
+
+<div id="wrap">
+<nav class="top-menu" role="navigation">
+  <div class="container-fluid">
+    <!-- Brand and toggle get grouped for better mobile display -->
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+      <a class="navbar-brand" href="#">Brand</a>
+    </div>
+
+    <!-- Collect the nav links, forms, and other content for toggling -->
+    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+      <ul class="nav navbar-nav">
+	<li> <a href="index.php" target="_self">Home</a></li>
+    <li> <a href="about.php" target="_self">About Us</a></li>
+    <li> <a href="contact.php" target="_self">Contact Us</a></li>
+    <li> <a href="profile.php" target="_self">Profile</a></li>
+    <li> <a href="services.php" target="_self">Services</a></li>
+  </ul>
+    </div><!-- /.navbar-collapse -->
+  </div><!-- /.container-fluid -->
+</nav>
+    
