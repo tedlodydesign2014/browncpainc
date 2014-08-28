24fe918334b78dadde2f80d8291a5ed8eaa72f6d
tedlodydesign2014 <tedlodydesign2014@gmail.com>
2014-08-28 05:28:01 -0500

    Updated Brown, included Bower Update

--- about.php
+++ about.php
@@ -1,5 +1,24 @@
 <?php require_once ('include/top.php'); ?>
 
+<section id="about">
+
+	<h3>About Us</h3>
+	<p>Our company is based on the belief that our customers' needs are of the utmost importance. We are committed to meeting those needs. As a result, a high percentage of our business is from repeat customers and referrals.</p>
+ 
+<p>We would welcome the opportunity to earn your trust and deliver you the best service in the industry.</p>
+ 
+<p>Location:</p>
+<p>17930 Lincoln Ct</p>
+<p>Lowell, In 46356</p>
+ 
+<p>Email: meganbrown@browncpainc.com</p>
+ 
+<p>Phone: (219) 613-9081</p>
+   
+</section>
+    	
+
+
 
 
 
